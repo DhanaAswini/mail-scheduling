d96f7ddd4502ad0329b84a73b1f3bac4bfec815a
DhanaAswini <dhanaaswinii@gmail.com>
2025-02-02 16:17:21 +0530

    3 commit

---
+++ app/components/ScheduledMail.js
@@ -0,0 +1,15 @@
+"use client";
+const ScheduledMail = ({ mailings }) => {
+  return (
+    <div>
+      <h2>Scheduled Mailings</h2>
+      <ul>
+        {mailings.map((mailing) => (
+          <li key={mailing.id}>
+            {mailing.mailingName}-{mailing.listName}-{mailing.schedule}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
