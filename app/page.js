241f3d96ae5f503b2bbe72cd75e15628e17b4d45
DhanaAswini <dhanaaswinii@gmail.com>
2025-02-02 16:37:50 +0530

    6 commit

--- app/page.js
+++ app/page.js
@@ -3,3 +3,25 @@ import { useState } from "react";
 import ScheduledMailForm from "./components/ScheduledMailForm";
 import ScheduledMail from "./components/ScheduledMail";
 import { fetchMailers, fetchLists } from "../utils/mockApi";
+
+export default function Home() {
+  const [mailings, setMailings] = useState([]);
+
+  const handleSchedule = (date) => {
+    const newMailing = {
+      id: mailings.length + 1,
+      mailerName: fetchMailers().find((m) => m.id === data.mailerId).name,
+      listName: fetchLists().find((l) => l.id === data.listId).name,
+      schedule: data.schedule,
+    };
+    setMailings([...mailings, newMailing]);
+  };
+
+  return (
+    <div>
+      <h1>Mailing Scheduler</h1>
+      <ScheduledMailForm onSubmit={handleSubmit} />
+      <ScheduledMail mailings={mailings} />
+    </div>
+  );
+}
