1e831b50edd2f7c4f8be0f2dbf85c0938fcb17e2
DhanaAswini <dhanaaswinii@gmail.com>
2025-02-02 16:23:30 +0530

    4 commit

---
+++ app/page.js
@@ -0,0 +1,5 @@
+"use client";
+import { useState } from "react";
+import ScheduledMailForm from "./components/ScheduledMailForm";
+import ScheduledMail from "./components/ScheduledMail";
+import { fetchMailers, fetchLists } from "../utils/mockApi";
---
+++ utils/mockApi.js
@@ -0,0 +1,13 @@
+export const fetchMailers = async () => {
+  return [
+    { id: 1, name: "Welcome Email" },
+    { id: 2, name: "Promotional Email" },
+  ];
+};
+
+export const fetchLists = async () => {
+  return [
+    { id: 1, name: "Subscribers" },
+    { id: 2, name: "Premium Users" },
+  ];
+};
